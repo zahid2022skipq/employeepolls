7b569b7967dc6bad1802a1bace0e2de06bdd8884
zahid2022slipq <zahid.ali.skipq@gmail.com>
2022-09-12 18:16:00 +0500

    Login Backend Pending

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
+import { useState } from "react";
 import "./App.css";
+import Home from "./Components/Home";
 import LoginForm from "./Components/LoginForm";
 
 function App() {
+  const [sub, setSub] = useState(false);
+
+  const setSubmission = (val) => {
+    setSub(val);
+    return val;
+  };
+
   return (
     <div className="container">
       <h1>Employee Polls</h1>
-      <LoginForm />
+      <LoginForm setSubmission={setSubmission} />
     </div>
   );
 }
--- src/Components/LoginForm.js
+++ src/Components/LoginForm.js
@@ -1,22 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import loginpng from "../Assets/login.png";
 import "../Components/login.css";
-function LoginForm() {
-    const 
-  const handleLogin = () => {};
+import Home from "./Home";
+function LoginForm(props) {
+  const { uname, pass } = [1, 2];
+  const [isSubmit, setIsSubmit] = useState(false);
+  const handleLogin = () => {
+    if (uname === "zahid" && pass === "zahid") {
+      setIsSubmit(true);
+    }
+  };
   return (
     <div className="login-container">
       <div className="login-img">
         <img src={loginpng} alt="login" height={100} width={100} />
       </div>
       <div className="form">
-        <form onSubmit={handleLogin}></form>
-        <h2>Login</h2>
-        <label>User Name</label>
-        <input type="text" name="user" placeholder="Enter username" />
-        <label>Password</label>
-        <input type="password" name="password" placeholder="Enter Password" />
-        <input type="submit" />
+        <form onSubmit={handleLogin}>
+          <h2>Login</h2>
+          <label>User Name</label>
+          <input type="text" name="user" placeholder="Enter username" />
+          <label>Password</label>
+          <input type="password" name="password" placeholder="Enter Password" />
+          <input type="submit" to="/home" />
+        </form>
       </div>
     </div>
   );
