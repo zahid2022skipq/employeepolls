f6f3fc6f863e408ce03000b67f6c7fbb9c735b9c
zahid2022slipq <zahid.ali.skipq@gmail.com>
2022-09-13 13:15:19 +0500

    Home Design Pending

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,12 +1,29 @@
 import React from "react";
 import Navigation from "./Navigation";
-
+import "./home.css";
 function Home() {
+  const users = [
+    { user: "test1", date: "4:00PM 09/13/2021" },
+    { user: "test2", date: "4:00PM 09/13/2021" },
+    { user: "test3", date: "4:00PM 09/13/2021" },
+    { user: "test4", date: "4:00PM 09/13/2021" },
+    { user: "test5", date: "4:00PM 09/13/2021" },
+  ];
   return (
-    <div>
+    <>
       <Navigation />
-      <h1>Home</h1>
-    </div>
+      <div className="home-container">
+        <h1>New Questions</h1>
+        <div className="card-container">
+          {users.map((u) => (
+            <div className="card">
+              {u.user} <p>{u.date}</p>
+              <button>Show</button>
+            </div>
+          ))}
+        </div>
+      </div>
+    </>
   );
 }
 
--- src/Components/Navigation.js
+++ src/Components/Navigation.js
@@ -1,19 +1,32 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "./nav.css";
+import profile from "../Assets/profile.png";
 function Navigation() {
   return (
-    <div className="container">
-      <Link className="link active" to="/home">
-        Home
-      </Link>
-      <Link className="link" to="/leaderboard">
-        Leaderboard
-      </Link>
-      <Link className="link" to="/new">
-        New
-      </Link>
-    </div>
+    <>
+      <div className="container">
+        <Link className="link active" to="/home">
+          Home
+        </Link>
+        <Link className="link" to="/leaderboard">
+          Leaderboard
+        </Link>
+        <Link className="link" to="/new">
+          New
+        </Link>
+
+        <div className="profile">
+          <img src={profile} height={30} width={30} alt="profile" />
+        </div>
+        <label style={{ marginLeft: "-180px", marginTop: "10px" }}>
+          zahidali
+        </label>
+        <Link className="link" to="/">
+          Logout
+        </Link>
+      </div>
+    </>
   );
 }
 
