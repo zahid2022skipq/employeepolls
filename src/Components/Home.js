847315086bae4d57fc065bf7239a9b418c474779
zahid2022slipq <zahid.ali.skipq@gmail.com>
2022-09-13 13:32:37 +0500

    Home > Questions done > Done Container (UI ONLY)

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -3,11 +3,11 @@ import Navigation from "./Navigation";
 import "./home.css";
 function Home() {
   const users = [
-    { user: "test1", date: "4:00PM 09/13/2021" },
-    { user: "test2", date: "4:00PM 09/13/2021" },
-    { user: "test3", date: "4:00PM 09/13/2021" },
-    { user: "test4", date: "4:00PM 09/13/2021" },
-    { user: "test5", date: "4:00PM 09/13/2021" },
+    { user: "test1", date: "4:00PM 09/13/2021", done: true },
+    { user: "test2", date: "4:00PM 09/13/2021", done: false },
+    { user: "test3", date: "4:00PM 09/13/2021", done: true },
+    { user: "test4", date: "4:00PM 09/13/2021", done: false },
+    { user: "test5", date: "4:00PM 09/13/2021", done: true },
   ];
   return (
     <>
@@ -15,12 +15,28 @@ function Home() {
       <div className="home-container">
         <h1>New Questions</h1>
         <div className="card-container">
-          {users.map((u) => (
-            <div className="card">
-              {u.user} <p>{u.date}</p>
-              <button>Show</button>
-            </div>
-          ))}
+          {users.map((u) =>
+            !u.done ? (
+              <div className="card">
+                {u.user} <p>{u.date}</p>
+                <button>Show</button>
+              </div>
+            ) : null
+          )}
+        </div>
+      </div>
+
+      <div className="done-container">
+        <h1>Done</h1>
+        <div className="card-container">
+          {users.map((u) =>
+            u.done ? (
+              <div className="card">
+                {u.user} <p>{u.date}</p>
+                <button>Show</button>
+              </div>
+            ) : null
+          )}
         </div>
       </div>
     </>
