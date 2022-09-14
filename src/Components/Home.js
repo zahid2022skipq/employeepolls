9245497827525374cad054cd3cbd5283d453df6b
zahid2022slipq <zahid.ali.skipq@gmail.com>
2022-09-14 19:15:43 +0500

    Bug Handling

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,15 +1,67 @@
-import React from "react";
+import React, { useState } from "react";
 import Navigation from "./Navigation";
 import "./home.css";
 
+const users = [
+  {
+    user: "test1",
+    date: "4:00PM 09/13/2021",
+    done: true,
+    image: "../Assets/gamer.png",
+  },
+  {
+    user: "test2",
+    date: "4:00PM 09/13/2021",
+    done: false,
+    image: "../Assets/manager.png",
+  },
+  {
+    user: "test3",
+    date: "4:00PM 09/13/2021",
+    done: true,
+    image: "../Assets/gamer.png",
+  },
+  {
+    user: "test4",
+    date: "4:00PM 09/13/2021",
+    done: false,
+    image: "../Assets/manager.png",
+  },
+  {
+    user: "test5",
+    date: "4:00PM 09/13/2021",
+    done: true,
+    image: "../Assets/manager.png",
+  },
+];
+
+const Dashboard = (props) => {
+  const close = () => {
+    props.close();
+  };
+  return (
+    <div className="dashboard">
+      <h1>Polls by {props.name}</h1>
+      <img src={props.image} height={100} width={100} alt="profile" />
+      <button onClick={close}>Close</button>
+    </div>
+  );
+};
+
 function Home() {
-  const users = [
-    { user: "test1", date: "4:00PM 09/13/2021", done: true },
-    { user: "test2", date: "4:00PM 09/13/2021", done: false },
-    { user: "test3", date: "4:00PM 09/13/2021", done: true },
-    { user: "test4", date: "4:00PM 09/13/2021", done: false },
-    { user: "test5", date: "4:00PM 09/13/2021", done: true },
-  ];
+  const [isOpen, setIsOpen] = useState(false);
+
+  const open = () => {
+    setIsOpen(true);
+  };
+
+  const close = () => {
+    setIsOpen(false);
+  };
+
+  const [name, setName] = useState("");
+  const [img, setImage] = useState("");
+
   return (
     <>
       <Navigation />
@@ -20,13 +72,17 @@ function Home() {
             !u.done ? (
               <div className="card" key={u.user}>
                 {u.user} <p>{u.date}</p>
-                <button>Show</button>
+                <button
+                  onClick={open}
+                  onFocus={setName(u.user)(setImage(u.image))}
+                >
+                  Show
+                </button>
               </div>
             ) : null
           )}
         </div>
       </div>
-
       <div className="done-container">
         <h1>Done</h1>
         <div className="card-container">
